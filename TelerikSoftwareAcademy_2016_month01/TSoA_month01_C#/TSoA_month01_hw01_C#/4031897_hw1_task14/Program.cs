c5f4947eb044056bbb9c9bb0aab26abddc0a7187
maginikolova <magi.nikolova@gmail.com>
2016-04-11 19:54:10 +0300

    Uploading new HW

---
+++ TelerikSoftwareAcademy_2016_month01/TSoA_month01_C#/TSoA_month01_hw01_C#/4031897_hw1_task14/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            DateTime timeNow = DateTime.Now;
+            Console.WriteLine(timeNow);
+        }
+    }
