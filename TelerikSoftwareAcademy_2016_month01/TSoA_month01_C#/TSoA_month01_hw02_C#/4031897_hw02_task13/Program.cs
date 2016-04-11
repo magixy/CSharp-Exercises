b0de0c6f8ef216aaf2b63a9d754468e074a2eefe
maginikolova <magi.nikolova@gmail.com>
2016-04-11 19:47:49 +0300

    Uploading new HW

---
+++ TelerikSoftwareAcademy_2016_month01/TSoA_month01_C#/TSoA_month01_hw02_C#/4031897_hw02_task13/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double eps = 0.000001;
+        double x = double.Parse(Console.ReadLine());
+        double y = double.Parse(Console.ReadLine());
+
+        if ( Math.Abs((x - y)) < eps )
+            Console.WriteLine("true");
+        else
+            Console.WriteLine("false");
+    }
+}
---
+++ TelerikSoftwareAcademy_2016_month01/TSoA_month01_C#/TSoA_month01_hw02_C#/4031897_hw02_task14/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        char a;
+        for (int i = 33; i < 127 ; i++) {
+            a = (char)i;
+            Console.Write(a);
+        }
+    }
+}
